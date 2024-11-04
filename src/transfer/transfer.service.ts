e1da863c74a7f9e80a7209b1f5ea3126d0905309
gian10carlos <gc2130925@gmail.com>
2024-11-03 23:58:01 -0500

    🐛 fix(transfer): update schema

--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -4,51 +4,26 @@ import { CreateTransferDto } from "./dto";
 
 @Injectable()
 export class TransferService {
-    constructor(
-        private readonly prisma: PrismaService,
-    ) { }
+    constructor(private readonly prisma: PrismaService) { }
 
     async create(createTransferDto: CreateTransferDto) {
         try {
-            const messageData =
-                createTransferDto.message?.trim() !== ""
-                    ? {
-                        create: {
-                            message: createTransferDto.message,
-                        },
-                    }
-                    : undefined;
+            const messageData = createTransferDto.message?.trim()
+                ? { create: { message: createTransferDto.message } }
+                : undefined;
 
             const transfer = await this.prisma.transfer.create({
                 data: {
-                    sender: {
-                        connect: {
-                            id: createTransferDto.id_sender,
-                        },
-                    },
-                    recipient: {
-                        connect: {
-                            id: createTransferDto.id_recipient,
-                        },
-                    },
+                    sender: { connect: { id: createTransferDto.id_sender } },
+                    recipient: { connect: { id: createTransferDto.id_recipient } },
                     amount: createTransferDto.amount,
                     date: new Date(createTransferDto.date),
                     messages: messageData,
                 },
                 select: {
                     id: true,
-                    sender: {
-                        select: {
-                            id: true,
-                            dni: true,
-                        },
-                    },
-                    recipient: {
-                        select: {
-                            id: true,
-                            dni: true,
-                        },
-                    },
+                    sender: { select: { id: true, dni: true } },
+                    recipient: { select: { id: true, dni: true } },
                     amount: true,
                 },
             });
@@ -56,7 +31,7 @@ export class TransferService {
             return transfer;
         } catch (error) {
             console.error(error);
-            throw new Error(error);
+            throw new Error('Error al crear la transferencia');
         }
     }
 
@@ -77,5 +52,4 @@ export class TransferService {
 
         return { transfers };
     }
-
 }
